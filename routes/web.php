c583a318d076355b8f65a9a822797d13a266d69d
agglnxz <140498082+agglnxz@users.noreply.github.com>
2024-02-13 14:41:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,25 @@ use App\Http\Controllers\ProfileController;
 
 Route::get('/',[WelcomeController::class,'index'])->name('dashboard');
 
-// login
-Route::get('/login',[LoginController::class,'index'])->name('login');
-Route::post('/login-proses',[LoginController::class,'login_proses'])->name('login-proses');
-Route::get('/logout',[LoginController::class,'logout'])->name('logout');
+Route::middleware(['guest'])->group(function () {
+  // login
+    Route::get('/login',[LoginController::class,'index'])->name('login');
+    Route::post('/login-proses',[LoginController::class,'login_proses'])->name('login-proses');
+    Route::get('/logout',[LoginController::class,'logout'])->name('logout');
 
-// register
-Route::get('/register',[RegisterController::class,'register'])->name('register');
-Route::post('/register-proses',[RegisterController::class,'register_proses'])->name('register-proses');
+  // register
+    Route::get('/register',[RegisterController::class,'register'])->name('register');
+    Route::post('/register-proses',[RegisterController::class,'register_proses'])->name('register-proses');
 
+});
 
 // photo
 Route::get('/photo.index',[PhotoController::class,'index'])->name('photo.index');
 
-// profile
-Route::get('/profile.index',[ProfileController::class,'index'])->name('profile.index');
-Route::get('/profile.edit',[ProfileController::class,'edit'])->name('profile.edit');
-Route::post('/profile.update',[ProfileController::class,'update'])->name('profile.update');
+Route::middleware(['auth'])->group(function () {
+  // profile
+    Route::get('/profile.index',[ProfileController::class,'index'])->name('profile.index');
+    Route::get('/profile.edit',[ProfileController::class,'edit'])->name('profile.edit');
+    Route::post('/profile.update',[ProfileController::class,'update'])->name('profile.update');
+
+});
