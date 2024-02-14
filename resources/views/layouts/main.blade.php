71aaa049606949a4b284ba70efa21b04497cbfd9
agglnxz <140498082+agglnxz@users.noreply.github.com>
2024-02-14 20:30:21 +0700

    UPDATE JS SALAH PANGGON ANJ

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -69,14 +69,14 @@
 
 
 	<!--====== Javascripts & Jquery ======-->
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/circle-progress.min.js"></script>
-	<script src="js/imagesloaded.pkgd.min.js"></script>
-	<script src="js/isotope.pkgd.min.js"></script>
 	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/jquery.nicescroll.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nicescroll.min.js"></script>
+	<script src="js/isotope.pkgd.min.js"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
 
 	</body>
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="header-warp">
         <a href="" class="site-logo">
-            <img src="./img/logo.png" alt="">
+            <img src="img/logo.png" alt="">
         </a>
         <img src="img/menu-icon.png" alt="" class="menu-icon">
         <ul class="main-menu">
@@ -17,7 +17,7 @@
             <li><a href="{{ route('logout') }}">Logout</a></li>
         @else
             <!-- Jika pengguna belum login -->
-            <li class="{{ request()->is('login') ? 'active' : '' }}"><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('login') }}">Login</a></li>
         @endif
         </ul>
         {{-- <div class="social-links-warp">
